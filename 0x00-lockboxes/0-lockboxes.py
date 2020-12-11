a187323253b11b4d53886a8a245f90e63c406c18
javier <javier>
2020-12-11 05:00:31 +0000

    creating algorithm

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,1 +1,17 @@
-def canUnlockAll(boxes)
+#!/usr/bin/python3
+"""
+Determines if all the boxes can be opened
+"""
+
+
+def canUnlockAll(boxes):
+    """ Check boxes """
+    keys = [0]
+    for k in keys:
+        openbox = boxes[k]
+        for nk in openbox:
+            if nk not in keys and nk < len(boxes):
+                keys.append(nk)
+    if len(keys) == len(boxes):
+        return True
+    return False
