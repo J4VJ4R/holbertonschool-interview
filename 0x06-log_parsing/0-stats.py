035c8c3edb554d712f14584d6bdb536596ea464d
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2021-01-29 09:24:45 -0500

    fix 3 all code

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,41 +1,54 @@
-             #!/usr/bin/python3
-             """This documents gather stats from stdin"""
-             import sys
+#!/usr/bin/python3
+"""Script that reads stdin line by line and computes metrics"""
+
+import sys
+
+data = {"Size": 0, "Codes": {"200": 0, "301": 0, "400": 0, "401": 0,
+                             "403": 0, "404": 0, "405": 0, "500": 0}}
 
 
-             def print_pretty(size, code_dict):
-                 """parse important data"""
-                 print("File size: {}".format(size))
-                 for key, value in sorted(code_dict.items()):
-                     if (value != 0):
-                         print("{}: {}".format(key, value))
+def printFormat():
+    """Function that prints the file size and all the status codes"""
 
-             if __name__ == '__main__':
-                 """init code to print the parsed data"""
-                 size = 0
-                 line_counter = 0
-                 code_dict = {
-                     "200": 0,
-                     "301": 0,
-                     "400": 0,
-                     "401": 0,
-                     "403": 0,
-                     "404": 0,
-                     "405": 0,
-                     "500": 0
-                 }
-                 try:
-                     for line in sys.stdin:
-                         line_counter += 1
-                         if (len(line.split()) < 2):
-                             continue
-                         code = line.split()[-2]
-                         size += int(line.split()[-1])
-                         if code in code_dict:
-                             code_dict[code] += 1
-                         if (line_counter % 10 == 0):
-                             print_pretty(size, code_dict)
-                     print_pretty(size, code_dict)
-                 except KeyboardInterrupt:
-                     print_pretty(size, code_dict)
-                     raise
+    print("File size: {}".format(data['Size']))
+
+    for statusCode, count in sorted(data['Codes'].items()):
+        if count != 0:
+            print("{}: {}".format(statusCode, count))
+
+
+if __name__ == "__main__":
+
+    try:
+
+        lines = 1
+        for line in sys.stdin:
+
+            """Split the line taken from stdin and save only the needed data"""
+
+            newLine = line.split(" ")
+            try:
+                size = int(newLine[-1])
+                code = newLine[-2]
+
+                """If the status code is a possible status code
+                it will count + 1 in the dict to count how many of they are"""
+
+                if code in data['Codes']:
+                    data['Codes'][code] += 1
+
+                data['Size'] += size
+            except:
+                pass
+
+            if lines % 10 == 0:
+                printFormat()
+
+            lines += 1
+
+    except KeyboardInterrupt:
+        """Handling CTRL + C"""
+        printFormat()
+        raise
+
+    printFormat()
