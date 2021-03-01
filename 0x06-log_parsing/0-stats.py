1b40bb4593b6c76976f2821d8b2f0dac4aceada8
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2021-03-01 09:38:48 -0500

    palindrome integer

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,54 +1,44 @@
 #!/usr/bin/python3
-"""Script that reads stdin line by line and computes metrics"""
+"""
+0x00. Minimum operations
+Script that reads stdin line by line and computes metrics
+Input: <IP> - [<date>] "GET /projects/260 HTTP/1.1" <stat code> <file size>
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning.
+"""
+from sys import stdin
 
-import sys
 
-data = {"Size": 0, "Codes": {"200": 0, "301": 0, "400": 0, "401": 0,
-                             "403": 0, "404": 0, "405": 0, "500": 0}}
+total_size = 0
+stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+         '404': 0, '405': 0, '500': 0}
 
 
-def printFormat():
-    """Function that prints the file size and all the status codes"""
-
-    print("File size: {}".format(data['Size']))
-
-    for statusCode, count in sorted(data['Codes'].items()):
-        if count != 0:
-            print("{}: {}".format(statusCode, count))
+def print_stats():
+    """
+    Print these statistics from the beginning
+    :return: None
+    """
+    print("File size: {}".format(total_size))
+    for key, value in sorted(stats.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
 
 
 if __name__ == "__main__":
-
+    line_counter = 0
     try:
-
-        lines = 1
-        for line in sys.stdin:
-
-            """Split the line taken from stdin and save only the needed data"""
-
-            newLine = line.split(" ")
-            try:
-                size = int(newLine[-1])
-                code = newLine[-2]
-
-                """If the status code is a possible status code
-                it will count + 1 in the dict to count how many of they are"""
-
-                if code in data['Codes']:
-                    data['Codes'][code] += 1
-
-                data['Size'] += size
-            except:
-                pass
-
-            if lines % 10 == 0:
-                printFormat()
-
-            lines += 1
-
+        for line in stdin:
+            args = line.split()
+            line_counter += 1
+            if len(args) > 2:
+                total_size += int(args[-1])
+                if args[-2] in stats:
+                    stats.update({str(args[-2]): stats.get(args[-2]) + 1})
+            if line_counter == 10:
+                print_stats()
+                line_counter = 0
     except KeyboardInterrupt:
-        """Handling CTRL + C"""
-        printFormat()
+        print_stats()
         raise
-
-    printFormat()
+    print_stats()
